eca51314140c810f49fadf59f321802e20e6737f
Jacob Chaulk <39381505+Visteus@users.noreply.github.com>
2026-07-18 10:46:08 -0500

    initial project/gradle setup

---
+++ src/main/java/com/visteus/sporebreach/SporeContainmentBreach.java
@@ -0,0 +1,13 @@
+package com.visteus.sporebreach;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(SporeContainmentBreach.MODID)
+public class SporeContainmentBreach {
+
+    public static final String MODID = "spore_containment_breach";
+
+    public SporeContainmentBreach(IEventBus modBus) {
+    }
+}
